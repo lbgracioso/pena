0174232c38d3b6f0ca16b0a07867cab7d55e323a
Lucas <contact@lbgracioso.net>
2025-04-08 11:16:22 -0300

    feat: Implement dark/light themes
    Signed-off-by: Lucas <contact@lbgracioso.net>

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,27 +1,44 @@
 import './css/Header.css';
 import I18n from "./components/i18n";
 import {useTranslation} from "react-i18next";
+import {useEffect, useState} from "react";
+import {FaSun, FaMoon} from "react-icons/fa";
 
 function Header() {
 
     const {t} =  useTranslation();
+
+    const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "dark");
+
+        useEffect(() => {
+            document.body.className = theme;
+            localStorage.setItem("theme", theme);
+        }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(prev => prev === "light" ? "dark" : "light");
+    };
 
     return (
         <header className="header">
             <h1>Pena</h1>
             <nav>
                 <ul>
+                    <li><I18n /></li>
                     <li>
-                        <I18n />
+                        <button onClick={toggleTheme} className="theme-toggle">
+                            {theme === "light" ? <FaMoon /> : <FaSun />}
+                        </button>
                     </li>
                     <li><a href="https://github.com/lbgracioso/pena">{t("Contribute")}</a></li>
                     <li>
-                        <iframe src="https://github.com/sponsors/lbgracioso/button" title={t("Sponsor")} height="32" width="114"></iframe>
+                        <iframe src="https://github.com/sponsors/lbgracioso/button"
+                                title={t("Sponsor")} height="32" width="114"></iframe>
                     </li>
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Header;
